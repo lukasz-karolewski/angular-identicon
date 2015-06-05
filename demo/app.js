5a5282ff6e41c24c5c7940f401c44ff28f908e31
kether667 <lkarolewski@gmail.com>
2015-06-04 18:17:01 -0700

    updated demo

--- demo/app.js
+++ demo/app.js
@@ -1,4 +1,19 @@
 angular.module('demo', ['ui.identicon'])
-    .controller('demoCtrl', function ($scope) {
-        $scope.test = 'asd';
+    .controller('demoCtrl', function ($scope, $timeout) {
+        $scope.username = '';
+        $scope.size = 300;
+
+
+        function type(text, letterDelay) {
+            $timeout(function () {
+                $scope.username += text.substring(0, 1);
+
+                var remainingText = text.substring(1, text.length);
+                if (remainingText) {
+                    type(remainingText, letterDelay);
+                }
+            }, letterDelay);
+        }
+
+        type('someemail@gmail.com', 500);
     });
