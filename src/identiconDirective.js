9c95954b3ab5aa79e5f2cdd46a2e9a28cfd357af
Bob Anderson <rpanderson@ra.rockwell.com>
2015-11-19 15:52:17 -0500

    Picks next best natural size based on size
    User can still provide their own, but it calculates it if it is not
    provided. The pattern for near perfect identicons starting with 10 are:
    10, 17, 22, 29, 34, 41, etc. So every 2 sizes are 12 apart. I add 12
    until we pass the provided $scope.size and subtract 5 to try to use the
    lower value if we can.
    
    The lowest natural size this is configured for is 17px. Theoretically if
    someone wanted one smaller than 10px, 10px would be a better natural
    size, but I assumed more people would be in the range of 24px minimum so
    it just cuts off an extra loop just in case (likely not even needed).
    But either way, I believe 15px is the minimum for identicons to even be
    useful.
    
    Also added one test in case you ever set up the framework (it doesn't
    appear to be set up right now).

--- src/identiconDirective.js
+++ src/identiconDirective.js
@@ -20,7 +20,12 @@ angular.module('ui.identicon', [])
             template: '<img width={{size}} height={{size}} ng-src="data:image/png;base64,{{data}}">',
             controller: function ($scope, md5) {
                 $scope.size = (typeof($scope.size) !== 'undefined' ? $scope.size : 24);
-                $scope.naturalSize = (typeof($scope.naturalSize) !== 'undefined' ? $scope.naturalSize : 300);
+                $scope.naturalSize = $scope.naturalSize;
+                if ((typeof($scope.naturalSize) === 'undefined')) {
+                    var n;
+                    for (n = 22; n < $scope.size; n+=12);
+                    $scope.naturalSize = ($scope.size < n - 5) ? n - 5 : n;
+                }
 
                 $scope.$watchGroup(['username', 'size'], function (newVal) {
                     $scope.data = new Identicon(md5.createHash($scope.username || ''), $scope.naturalSize).toString();
--- src/identiconDirectiveSpec.js
+++ src/identiconDirectiveSpec.js
@@ -6,11 +6,20 @@ describe('Directive: ui.identicon', function () {
     beforeEach(inject(function ($compile, $rootScope) {
         scope = $rootScope.$new();
 
-        ele = angular.element('<identicon hash="test@gmail.com" size="32"></identicon>');
+        ele = angular.element('<identicon username="test@gmail.com" size="32"></identicon>');
 
         $compile(ele)(scope);
         scope.$apply();
     }));
+
+    /**
+     * @description
+     * Ensures naturalSize calculation is correct based on provided size and
+     * no provided naturalSize.
+     * */
+    it('should calculate a valid natural size', function() {
+        expect($scope.naturalSize).toBe(34);
+    });
 
     /**
      * @description
